738fde683965f9003187e38f3fcfbe79662743ea
Keith Johnson <kj@ubergeek42.com>
2012-07-21 12:11:28 -0400

    Initial Commit

---
+++ rdpmanager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rdpmanager
+//
+//  Created by Keith Johnson on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ rdpmanager/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  rdpmanager
+//
+//  Created by Keith Johnson on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
